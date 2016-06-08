ef738cad976a192795fb302270e26b585437c8a4
Yoon Bae <bae.yoon@gmail.com>
2016-06-08 15:13:22 +0900

    각 모델을 admin에 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.humanize.templatetags.humanize import intcomma
+from .models import Category
+from .models import Shop
+from .models import Review
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ['title']
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display= ['title', 'phone', 'created_at']
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display= ['title', 'username', 'created_at']
+
+admin.site.register(Post, PostAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    categoryname = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Shop(models.Model):
-    shopname = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     content = models.TextField()
@@ -17,8 +17,11 @@ class Shop(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Review(models.Model):
-    shopname = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     username = models.CharField(max_length=15)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.title
